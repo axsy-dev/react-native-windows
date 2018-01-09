47a6e84e77d876289e986e71bed566d054b2ac8d
Radu Simionescu <reseul@hotmail.com>
2018-01-09 08:36:41 -0800

    Fix DependencyObjectData leak (#1561)
    DependencyObjectExtensions maintains DependencyObjectData objects attached to each native view.
    There is a ClearData cleanup method that's supposed to be called when a view is destroyed, but the current code doesn't do that.
    As a result memory usage increases during the life time of an application.
    The fix is to call ClearData from NativeViewHierarchyManager.DropView.
    
    This exposes some cases where some asynchronous completion of an operation initiated by a view happens after the view is unmounted. Added some checks to prevent exceptions in one of these cases.
    There may be other cases as well!
    
    A more ideal solution would do the cleanup when the view reaches a reference count of 0, but that would require some major changes of the view lifetime, etc.

--- ReactWindows/ReactNative.Net46/Views/Image/ReactImageManager.cs
+++ ReactWindows/ReactNative.Net46/Views/Image/ReactImageManager.cs
@@ -1,4 +1,4 @@
-﻿using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Linq;
 using ReactNative.Collections;
 using ReactNative.Modules.Image;
 using ReactNative.UIManager;
@@ -245,6 +245,12 @@ namespace ReactNative.Views.Image
 
         private void OnImageFailed(Border view)
         {
+            if (!view.HasTag())
+            {
+                // View may have been unmounted, ignore.
+                return;
+            }
+
             view.GetReactContext()
                 .GetNativeModule<UIManagerModule>()
                 .EventDispatcher
@@ -256,6 +262,12 @@ namespace ReactNative.Views.Image
 
         private void OnImageStatusUpdate(Border view, ImageStatusEventData status)
         {
+            if (!view.HasTag())
+            {
+                // View may have been unmounted, ignore.
+                return;
+            }
+
             var eventDispatcher = view.GetReactContext()
                 .GetNativeModule<UIManagerModule>()
                 .EventDispatcher;
